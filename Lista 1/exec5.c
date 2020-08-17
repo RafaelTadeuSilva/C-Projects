cac9e18e38d01159c32feaeba2e58a845ae5952b
RafaelTadeuSilva <56327128+RafaelTadeuSilva@users.noreply.github.com>
2020-08-17 07:03:16 -0300

    exec 5 to 7

--- Lista 1/exec5.c
+++ Lista 1/exec5.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 
 int main(){
+    float tempo, vel_med, rendimento, consumo;
+    printf("Exercicio 5\n");
+
+    printf("Informe o tempo da viagem em horas: ");
+    scanf("%f", &tempo);
+
+    printf("Informe a velocidade media em km/h: ");
+    scanf("%f", &vel_med);
+
+    printf("Informe o rendimento do carro em km/l: ");
+    scanf("%f", &rendimento);
+
+    consumo = (vel_med * tempo)/rendimento;
+    
+    printf("O consumo sera de : %.2f litros", consumo);
+
     return 0;
 }
--- Lista 1/exec6.c
+++ Lista 1/exec6.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
 int main(){
+    float saldo;
+    printf("Exercicio 6\n");
+    printf("Informe o saldo atual: ");
+    scanf("%f", &saldo);
+    printf("Saldo com reajuste: %.2f", saldo*1.15);
     return 0;
 }
